5433dde72c0d8064831028dcd7c44696f30c2233
mwolgataran <olga.taran@corezoid.com>
2019-02-19 10:53:04 +0200

    Add signature to function

--- index.js
+++ index.js
@@ -11,18 +11,40 @@ class CorezoidRequest {
         return axios.post(this.url,{"ops":[{"type":"get", "obj":"obj_scheme", obj_id, obj_type}]});
     }
 
-    async addTask(conv_id, ref, task_data) {
-        return axios.post(this.url,{"ops":[{"type":"create", "obj":"task", "conv_id":conv_id, "ref":ref, "data": task_data}]});
+    async addTask(conv_id, ref, task_data, api_login, secret_key) {
+
+        const body = {
+            "ops": [{
+                "type": "create",
+                "obj": "task",
+                "conv_id": conv_id,
+                "ref": ref,
+                "data": task_data
+            }]
+        };
+
+        const unix = moment().unix();
+
+        let json_string = JSON.stringify(body);
+
+        const string = unix + secret_key + json_string + secret_key;
+
+        const sign = sha1Hex(string);
+
+        const url = "https://api.corezoid.com/api/2/json/" + api_login + "/" + unix + "/" + sign;
+
+        const response = await axios.post(url, body).then(res => res.data);
+
+        return response;
     }
 
-    getSign(secret_key, body){
+    getSign(unix, secret_key, body){
         const unix = moment().unix();
 
         let json_string = JSON.stringify(body);
         const string = unix + secret_key + json_string + secret_key;
-        
-        return sha1Hex(string);
 
+        return sha1Hex(string);
     }
 }
 
